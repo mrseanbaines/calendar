b0be10a9748d81f1c3399c98875bd7ce80d313e1
Sean Baines <mrseanbaines@gmail.com>
2018-12-20 15:31:37 +0000

    Refactor day props

--- src/components/Month.js
+++ src/components/Month.js
@@ -22,32 +22,32 @@ export default memo(({
   <div id={id}>
     {month.weeks.map((week, i) => (
       <Flex key={i} flexWrap='wrap'>
-        {week.map((day, i) => (
-          <Box key={i} width={1/7}>
-            <Day
-              handleDateSelect={handleDateSelect}
-              dayContents={day && getDate(day)}
-              isDaySelected={
-                isSameDay(day, startDate) ||
-                isSameDay(day, endDate) ||
-                (isBefore(day, endDate) && isAfter(day, startDate))
-              }
-              selectedStart={startDate && endDate && isSameDay(day, startDate)}
-              selectedEnd={startDate && endDate && isSameDay(day, endDate)}
-              selectedMiddle={
-                startDate && endDate &&
-                isBefore(day, endDate) && isAfter(day, startDate)
-              }
-              day={day}
-              handleDateHover={handleDateHover}
-              isDayHighlighted={
-                startDate && focusedDate === END_DATE &&
-                ((isBefore(day, hoveredDate) && isAfter(day, startDate)) ||
-                isSameDay(day, startDate) || isSameDay(day, hoveredDate))
-              }
-            />
-          </Box>
-        ))}
+        {week.map((day, i) => {
+          const isStartDate = isSameDay(day, startDate);
+          const isEndDate = isSameDay(day, endDate);
+          const isHoveredDate = isSameDay(day, hoveredDate);
+          const bothDatesSelected = startDate && endDate;
+          const isBetweenStartAnd = date => isBefore(day, date) && isAfter(day, startDate);
+
+          return (
+            <Box key={i} width={1/7}>
+              <Day
+                day={day}
+                dayContents={day && getDate(day)}
+                handleDateSelect={handleDateSelect}
+                handleDateHover={handleDateHover}
+                selectedStart={bothDatesSelected && isStartDate}
+                selectedEnd={bothDatesSelected && isEndDate}
+                selectedMiddle={bothDatesSelected && isBetweenStartAnd(endDate)}
+                isDaySelected={isStartDate || isEndDate || isBetweenStartAnd(endDate)}
+                isDayHighlighted={
+                  (startDate && focusedDate === END_DATE) &&
+                  (isBetweenStartAnd(hoveredDate) || isStartDate || isHoveredDate)
+                }
+              />
+            </Box>
+          );
+        })}
       </Flex>
     ))}
   </div>
